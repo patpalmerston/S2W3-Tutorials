5ee5e00073edca2b03e9c7485e1fcd34a99f57f6
Pat Palmerston <patpalmerston@gmail.com>
2019-07-22 15:04:43 -0700

    mvp

--- React-Router-Movies-master/client/src/App.js
+++ React-Router-Movies-master/client/src/App.js
@@ -1,20 +1,24 @@
 import React, { useState } from 'react';
+import { Route } from 'react-router-dom';
 
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 
 const App = () => {
-  const [savedList, setSavedList] = useState( [] );
+	const [savedList, setSavedList] = useState([]);
 
-  const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
-  };
+	const addToSavedList = movie => {
+		setSavedList([...savedList, movie]);
+	};
 
-  return (
-    <div>
-      <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
-    </div>
-  );
+	return (
+		<div>
+			<Route exact path='/' component={MovieList} />
+			<Route path='/movies/:id' component={Movie} />
+			<SavedList list={savedList} />
+		</div>
+	);
 };
 
 export default App;
--- React-Router-Movies-master/client/src/Movies/SavedList.js
+++ React-Router-Movies-master/client/src/Movies/SavedList.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const SavedList = props => (
-  <div className="saved-list">
-    <h3>Saved Movies:</h3>
-    {props.list.map(movie => (
-      <span className="saved-movie">{movie.title}</span>
-    ))}
-    <div className="home-button">Home</div>
-  </div>
+	<div className='saved-list'>
+		<h3>Saved Movies:</h3>
+		{props.list.map(movie => (
+			<span className='saved-movie'>{movie.title}</span>
+		))}
+		{/* <div className="home-button" onClick={}>Home</div> */}
+		<Link className='home-button' to='/'>
+			Home
+		</Link>
+	</div>
 );
 
 export default SavedList;
